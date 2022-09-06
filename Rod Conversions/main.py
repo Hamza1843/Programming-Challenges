e77babbbb4b7768f84bd516076e9bbc79567a7c5
Hamza1843 <99148795+Hamza1843@users.noreply.github.com>
2022-09-06 10:45:23 +0100

    Add files via upload

--- Rod Conversions/main.py
+++ Rod Conversions/main.py
@@ -8,3 +8,14 @@ def furlongs(rods):
     return(rods/40)
 def time_to_walk(rods):
     return(miles(rods)/3.1*60)
+
+rods = float(input("Input rods: "))
+print(f"You input {rods} rods.")
+print(f"""
+Conversions
+Meters: {meters(rods)}
+Feet: {feet(rods)}
+Furlongs: {furlongs(rods)}
+Minutes to walk 10.0 rods: {time_to_walk(rods)}
+""")
+
