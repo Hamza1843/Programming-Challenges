2fbb37f408abc31caa61e2a59206b3dbb5a31327
Hamza1843 <99148795+Hamza1843@users.noreply.github.com>
2022-09-06 10:38:42 +0100

    Add files via upload

--- Rod Conversions/testing.py
+++ Rod Conversions/testing.py
@@ -1,13 +1,20 @@
 import unittest
 
-from main_2 import *
+from main import *
 
 
 class MyFirstTests(unittest.TestCase):
 
     def test_hello(self):
-        self.assertEqual(meters(10), 50.292)
-
-    def test_custom_num_list(self):
-        self.assertEqual(len(create_list(10)), 10)
+        self.assertEqual(meters(100), 502.92)
+        self.assertEqual(meters(999999), 5029194.9708)
+        self.assertEqual(feet(10), 165)
+        self.assertEqual(feet(100), 1650)
+        self.assertEqual(feet(999999), 16499983.5)
+        self.assertEqual(miles(999999), 3125.004642151441)
+        self.assertEqual(furlongs(10), 0.25)
+        self.assertEqual(furlongs(100), 2.5)
+        self.assertEqual(furlongs(999999), 24999.975)
+        self.assertEqual(time_to_walk(999999), 60483.96081583434)
+        
         
