6fb23d114875dcee654550d9ace9d44be7e2c495
Hamza1843 <99148795+Hamza1843@users.noreply.github.com>
2022-09-12 09:40:28 +0100

    Add files via upload

--- Richter/main.py
+++ Richter/main.py
@@ -1,22 +1,24 @@
 def joules(richter):
     return 10**((1.5*richter)+4.8)
-def tnt(energy):
-    return energy/(4.184*10**9)
+def tnt(richter):
+    return joules(richter)/(4.184*10**9)
 def print_predefined_values():
     print(f"""
-Richter                 Joules                  TNT
-1                       {joules(1)}      {tnt(joules(1))}
-5                       {joules(5)}      {tnt(joules(5))}
-9.1                     {joules(9.1)}   {tnt(joules(9.1))}
-9.2                     {joules(9.2)}   {tnt(joules(9.2))}
-9.5                     {joules(9.5)}  {tnt(joules(9.5))}
+{"Richter":<24}{"Joules":<24}{"TNT":<24}
+{1:<24}{joules(1):<24}{tnt(1):<30}
+{5:<24}{joules(5):<24}{tnt(5):<30}
+{9.1:<24}{joules(9.1):<24}{tnt(9.1):<30}
+{9.2:<24}{joules(9.2):<24}{tnt(9.2):<30}
+{9.5:<24}{joules(9.5):<24}{tnt(9.5):<30}
 """)
 def user_input():
-    return(float(input(f"Please enter a richter scale value: ")))
+    return float(input(f"Please enter a richter scale value: "))
 def print_output(richter):
     print(f"""
 Richter value: {richter}
 Equivalence in joules: {joules(richter)}
-Equivalence in tons of TNT: {joules(richter)}
+Equivalence in tons of TNT: {tnt(richter)}
 """)
+print_predefined_values()
+print_output(user_input())
 
--- Richter/testing.py
+++ Richter/testing.py
@@ -6,11 +6,12 @@ from main import *
 class MyFirstTests(unittest.TestCase):
 
     def test_hello(self):
-        self.assertEqual(joules(1), 1995262.31497)
-        self.assertEqual(joules(3.4251), 8662634684.99)
-        self.assertEqual(tnt(10), 15080242351.8)
-        self.assertEqual(tnt(1), 0.00047687913)
-        self.assertEqual(tnt(3.4251), 2.07041937978)
+        self.assertEqual(joules(10), 6.309573444801943e+19)
+        self.assertEqual(joules(1), 1995262.3149688789)
+        self.assertEqual(joules(3.4251), 8662634684.989311)
+        self.assertEqual(tnt(10), 15080242458.895658)
+        self.assertEqual(tnt(1), 0.00047687913837688307)
+        self.assertEqual(tnt(3.4251), 2.07041937977756)
 
 if __name__ == '__main__':
     unittest.main()
